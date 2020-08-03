e4f5401135297e1eb554275079cbc04d2a4c0139
Rokas Puzonas <rokas.puz@gmail.com>
2020-08-03 17:10:14 +0300

    Added setup.py

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MineHost Interface
 
 A python library for executing commands or gettings information on MineHost minecraft servers.
+
+Documentation will be added soon.
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+setuptools.setup(
+	name="minehost-interface",
+	version="1.0.0",
+	author="Rokas Puzonas",
+	author_email="rokas.puz@gmail.com",
+	description="A simple for interacting with minehost servers.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/RokasPuzonas/minehost-interface",
+	license="MIT",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Topic :: Software Development",
+		"Topic :: Software Development :: Libraries",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Utilities",
+		"Intended Audience :: Developers",
+	],
+	install_requires=["beautifulsoup4", "requests", "paramiko"],
+	python_requires=">=3.8",
+)
